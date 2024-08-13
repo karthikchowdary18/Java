97ad2fc9fbfca02f8c3daa12176933bb5fe813f9
karthik chowdary nunna <karthikchowdary018@gmail.com>
2024-08-13 17:40:31 +0530

    Initial Commit for this project

---
+++ src/com/karthik/Main.java
@@ -0,0 +1,14 @@
+package com.karthik;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner sc =  new Scanner(System.in);
+//        System.out.println(sc.next());
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Do you remembered your number:");
+            int num = sc.nextInt();
+            System.out.println("your number is "+num);
+    }
+}
