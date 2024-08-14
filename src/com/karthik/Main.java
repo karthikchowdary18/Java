fb84c16fb203fdbab30dd7e651f13f3d73b36740
karthik chowdary nunna <karthikchowdary018@gmail.com>
2024-08-14 18:48:47 +0530

    Initial Commit for this project

--- src/com/karthik/Main.java
+++ src/com/karthik/Main.java
@@ -6,9 +6,26 @@ public class Main {
     public static void main(String[] args) {
 //        Scanner sc =  new Scanner(System.in);
 //        System.out.println(sc.next());
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Do you remembered your number:");
-            int num = sc.nextInt();
-            System.out.println("your number is "+num);
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Do you remembered your number:");
+//            int num = sc.nextInt();
+//            System.out.println("your number is "+num);
+
+        //type casting
+        int a = (int)(12.9f);
+        System.out.println(a);
+
+        //automatic type promotion in expressions
+        int e = 257;
+        byte b = (byte)(e);
+        System.out.println(b); //257 % 256 = 1
+
+        //java uses "UNICODE" principles
+        int num ='A'; //aacii values
+        int num2 = 'a'; //aacii values
+        System.out.println(num+num2);
+
+
+
     }
 }
